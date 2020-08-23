eb94674ca3f3e2deaee7c043e518191679e20a0c
Li Lin <guru.lin@gmail.com>
2020-08-23 14:00:52 +0800

    changed data model json property to match upstream service

--- src/main/java/demo/bank/customer/Customer.java
+++ src/main/java/demo/bank/customer/Customer.java
@@ -1,10 +1,13 @@
 package demo.bank.customer;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class Customer {
+    @JsonProperty("customer_id")
     private String customerId;
+    @JsonProperty("login_name")
     private String loginName;
     private String name;
 }
